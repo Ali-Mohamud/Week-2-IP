ff44159e10a6a39cb03ce98ba639730a299c7b0f
Ali-Mohamud <ali.mohamud@student.moringaschool.com>
2022-03-06 15:16:53 +0300

    feat: add the function that calculates & outputs the akan result names

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
 var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", " Yaa", "Afua", "Ama"];
 
+function akanNames() {
+    var year = document.getElementById("year").value;
+    var month = document.getElementById("month").value;
+    var day = document.getElementById("day").value;
+    var gender = document.getElementById("gender").value;
+    let birthday = new Date(`${year}-${month}-${day}`)
+    let dateString = birthday.toDateString()
+    let birthDate = birthday.getDay()
+  
+  
+    if (gender === "male") {
+      let name = maleNames[birthDate]
+      output.textContent = "You were born on " + dateString + ",  Your Akan Name is " + name
+    } else if (gender === "female") {
+      let name = femaleNames[birthDate]
+      output.textContent = "You were born on " + dateString + ",  Your Akan Name is " + name
+    } else {
+      alert("Select your gender")
+    }
+  
+    if (day > 31 || day < 1) {
+      alert("Invalid date")
+      output.textContent = "Put a valid date!"
+    }
+  
+    if (month > 12 || month < 1) {
+      alert("Invalid month")
+      output.textContent = "Put a valid month!"
+  
+    }
+  }
